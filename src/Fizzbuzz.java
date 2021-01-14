7c59315562801194452ce15ff528af90eba65838
limjoonchul <dlawnscjf511@naver.com>
2021-01-14 21:48:28 +0900

    feat: add fizzbuzz f to F

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -2,7 +2,7 @@ public class Fizzbuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 15 == 0){
-                System.out.println("fizzbuzz");
+                System.out.println("Fizzbuzz");
             }
             System.out.println(i);
         }
